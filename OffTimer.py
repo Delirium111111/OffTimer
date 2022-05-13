b7d010ef125bfcc3f165f9fc26d74bd2e4cdaf8e
Delirium111111 <hackerman1337228@mail.ru>
2022-05-13 17:23:20 +0300

    Slider Connection

--- OffTimer.py
+++ OffTimer.py
@@ -5,6 +5,7 @@
 # ------------------------------------------------------
 
 import sys
+from tkinter import S
 
 import qtmodern.styles
 import qtmodern.windows
@@ -14,13 +15,27 @@ from PySide6.QtWidgets import QApplication, QMainWindow
 from Window import Ui_MainWindow
 
 class OffTimer(QMainWindow):
+    timer_amount = 0
     def __init__(self):
         super(OffTimer, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.SyncLables(0)
+        self.ui.HS_Value.valueChanged.connect(self.SyncLables)
+    
+        
+
+    def TakeAmount(self) -> int:
+        return self.ui.HS_Value.value()
+
+    def SyncLables(self, value=TakeAmount):
+        self.ui.L_Value_text.setText(str(value))
+    
+
+
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-
     window = OffTimer()
 
     qtmodern.styles.dark(app)
